69deb914a992000dc90f492887ef5aa1809cd09c
xtinmo <xtinmo@mac.com>
2012-05-26 14:50:24 +0800

    Added beginning of tests

--- test/relativeclock_spec.rb
+++ test/relativeclock_spec.rb
@@ -1,12 +1,19 @@
 require 'singapore_relative_clock'
+#!/user/bin/ruby -w
+time=Time.new
+
+#Components of a Time 
 
 describe SingaporeRelativeClock do
 	before(:all) do
 		@clock = SingaporeRelativeClock.new
 	end
 
-	it "should..." do
+	it "should return -2 if the given city is Sydney" do
 
 	end
 
+	it "should return 0 if the given city is Singapore" do
+
+	end
 end
